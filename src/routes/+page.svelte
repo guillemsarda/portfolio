e2bc57da932ab360bbb375af6dc6df5b11a2c73e
Guillem Sardà <gsardaparreu@gmail.com>
2022-08-25 18:49:58 +0200

    feat: change title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,30 @@
-<h1>Guillem Sardà Parreu</h1>
+<script></script>
+
+<div class="main"> 
+  <h1>Guillem Sardà Parreu</h1>
+  <div class="clock">
+    <div class="wrap">
+      <span class="hour"></span>
+      <span class="minute"></span>
+      <span class="second"></span>
+      <span class="dot"></span>
+    </div>
+  </div>
+</div>
+
+<style>
+  .main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  h1 {
+    font-family: 'Courier New', Courier, monospace;
+    transition: 1.5s ease;
+  }
+  h1:hover {
+    color: #ff3e00;
+  }
+</style>
