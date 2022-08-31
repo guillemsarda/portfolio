bb1dbf73cd7304c993ddf0dd8dc5bad631c47a4f
Guillem Sardà <gsardaparreu@gmail.com>
2022-08-31 13:05:15 +0200

    feat: add animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 
 <div class="main"> 
   <h1>Guillem Sardà Parreu</h1>
+  <span class="animated">
+    <h2>Coming soon...</h2>
+  </span>
 </div>
 
 <style>
@@ -10,13 +13,50 @@
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    text-align: center;
     height: 100vh;
+    width: 100vw;
   }
   h1 {
     font-family: 'Courier New', Courier, monospace;
     transition: 1.5s ease;
+    justify-self: center;
   }
   h1:hover {
     color: #ff3e00;
   }
+  h2 {
+    font-family: 'Courier New', Courier, monospace;
+  }
+  .animated {
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    animation: bounceIn 5s;
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes bounceIn {
+   0% { 
+    opacity: 0; 
+    transform: translateX(-10vw);
+   }
+   10% {
+    opacity: 0.5;
+   }
+   35% {
+    /* transform: translateX(5vw); */
+    color: #ff3e00;
+    opacity: 1;
+   }
+   70% { 
+    opacity: 0;
+    transform: translateX(10vw);
+   }
+   100% {
+    opacity: 0;
+   }
+} 
 </style>
