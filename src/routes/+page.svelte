991376013a98f60d0799ed2cdf0faf9eb3f03f32
Gui <gsardaparreu@gmail.com>
2022-09-24 19:48:52 +0200

    feat: remove coming soon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
-<script></script>
+<script>
+	import Info from './components/info.svelte';
+</script>
 
 <main class="main">
 	<h1>Guillem Sardà Parreu</h1>
-	<span class="animated">
-		<h2>Coming soon...</h2>
-	</span>
+	<Info />
 </main>
 
 <style>
@@ -24,39 +24,5 @@
 	}
 	h1:hover {
 		color: #ff3e00;
-	}
-	h2 {
-		font-family: 'Courier New', Courier, monospace;
-	}
-	.animated {
-		width: 100vw;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		text-align: center;
-		animation: bounceIn 5s;
-		animation-iteration-count: infinite;
-	}
-
-	@keyframes bounceIn {
-		0% {
-			opacity: 0;
-			transform: translateX(-10vw);
-		}
-		10% {
-			opacity: 0.5;
-		}
-		35% {
-			/* transform: translateX(5vw); */
-			color: #ff3e00;
-			opacity: 1;
-		}
-		70% {
-			opacity: 0;
-			transform: translateX(10vw);
-		}
-		100% {
-			opacity: 0;
-		}
 	}
 </style>
---
+++ src/routes/components/info.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { fly, fade } from 'svelte/transition';
+	let visible = 'beginning';
+	function handleClick() {
+		switch (visible) {
+			case 'beginning':
+				visible = 'stack';
+				break;
+			case 'stack':
+				visible = 'next';
+				break;
+			default:
+				visible = 'beginning';
+				break;
+		}
+	}
+</script>
+
+<button class="info-button" on:click={handleClick}>
+	{#if visible === 'beginning'}
+		<p class="intro-content" in:fade={{ duration: 1000 }}>
+			I am a Software Developer (click for more).
+		</p>
+	{:else if visible === 'stack'}
+		<p class="intro-content" in:fly={{ x: 500, duration: 1000 }}>
+			I am proficient with JavaScript and TypeScript (click for more).
+		</p>
+	{:else if visible === 'next'}
+		<p class="intro-content" in:fly={{ x: -500, duration: 1000 }}>
+			Currently learning C. Stay tuned (click to end).
+		</p>
+	{/if}
+</button>
+
+<style>
+	.info-button {
+		width: auto;
+		background-color: transparent;
+		border: none;
+	}
+	.intro-content {
+		font-family: 'Courier New', Courier, monospace;
+	}
+</style>
