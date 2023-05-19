c975d2719c6507fc14c28c211a2d48b61e18ef8a
Gui <gsardaparreu@gmail.com>
2023-05-19 20:56:48 +0200

    feat: add grid

---
+++ src/routes/components/carrousel.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Project from './project.svelte';
+
+	const arr = [{ title: 'hello' }, { title: 'world' }, { title: 'hello' }, { title: 'hello' }];
+</script>
+
+<ul class="carrousel">
+	{#each arr as proj, i}
+		<Project {proj} {i} />
+	{/each}
+	<!-- <li class="project-container fa">h</li>
+	<li class="project-container s">h</li>
+	<li class="project-container t ">h</li>
+	<li class="project-container l">h</li> -->
+</ul>
+
+<style>
+	.carrousel {
+		border: red solid 1px;
+		display: grid;
+		grid-row: span 7;
+		grid-column: 2 / 10;
+		list-style-type: none;
+		grid-template-columns: repeat(4, 1fr);
+		grid-template-rows: repeat(4, 1fr);
+		row-gap: 5vh;
+		column-gap: 15vw;
+	}
+</style>
---
+++ src/routes/components/project.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { IProject } from 'src/utils/interfaces';
+	export let proj: IProject;
+</script>
+
+<li class="project-container">
+	<img src="placeholder-image.png" alt={proj.title} class="project-cover" />
+	<h1 class="project-title">{proj.title}</h1>
+</li>
+
+<style>
+	.project-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		border: blue solid 1px;
+		grid-column: span 2;
+		grid-row: span 2;
+	}
+	.project-cover {
+		width: 100%;
+		height: 80%;
+	}
+	.project-title {
+		height: 20%;
+	}
+</style>
