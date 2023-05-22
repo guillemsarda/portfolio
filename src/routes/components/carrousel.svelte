c61d8421a2078874db59f657c693f26905c607e0
Gui <gsardaparreu@gmail.com>
2023-05-22 19:58:59 +0200

    feat: add images

--- src/routes/components/carrousel.svelte
+++ src/routes/components/carrousel.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import Project from './project.svelte';
-
-	const arr = [{ title: 'hello' }, { title: 'world' }, { title: 'hello' }, { title: 'hello' }];
+	const arr = [
+		{
+			title: 'Parkpal',
+			url: 'https://parkpal-web-omega.vercel.app/',
+			img: '/logo.svg',
+			bgColor: 'rgb(113,69,214)'
+		},
+		{
+			title: 'react-rxjs-state-management',
+			url: 'https://www.npmjs.com/package/react-rxjs-state-management',
+			img: '/npm.svg',
+			bgColor: 'rgb(193,32,39)'
+		},
+		{ title: 'What2Do', url: '', img: '/w2d.png', bgColor: 'rgb(217,240,252)' },
+		{ title: 'Bienvenido Al Globo', url: 'www.bienvenidoalglobo.top', img: '/globo-logo.svg' }
+	];
 </script>
 
 <ul class="carrousel">
--- src/routes/components/project.svelte
+++ src/routes/components/project.svelte
@@ -4,23 +4,38 @@
 </script>
 
 <li class="project-container">
-	<img src="placeholder-image.png" alt={proj.title} class="project-cover" />
-	<h1 class="project-title">{proj.title}</h1>
+	<a href={proj.url} class="project-link">
+		<img
+			src={proj.img}
+			alt={proj.title}
+			class="project-cover"
+			style={`background-color: ${proj.bgColor}`}
+		/>
+		<h1 class="project-title">{proj.title}</h1>
+	</a>
 </li>
 
 <style>
 	.project-container {
+		grid-column: span 2;
+		grid-row: span 2;
+		border: blue solid 2px;
+	}
+	.project-link {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		border: blue solid 1px;
-		grid-column: span 2;
-		grid-row: span 2;
+		width: 100%;
+		height: 100%;
+		text-decoration: none;
+		color: inherit;
 	}
 	.project-cover {
-		width: 100%;
+		width: 80%;
 		height: 80%;
+		border: solid rgb(255, 0, 0) 1px;
+		object-fit: scale-down;
 	}
 	.project-title {
 		height: 20%;
